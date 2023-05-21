d346d47f3227f1be49abc21af4f247231964f4c1
Suleyman Guliyev <suleyman.guliyev11@gmail.com>
2023-05-21 23:56:06 +0300

    Initial commit

---
+++ js/questions.js
@@ -0,0 +1,38 @@
+let questions = [
+  {
+    numb: 1,
+    question: "In which country below is the Chernobyl nuclear plant located?",
+    answer: "Ukraine",
+    options: ["Russia", "Belarus", "Ukraine", "Kazakhstan"],
+  },
+  {
+    numb: 2,
+    question: "Which scientist below made a device for raising water ",
+    answer: "Archimedes",
+    options: ["Einstein", "Archimedes", "Nikola Tesla", "Newton"],
+  },
+  {
+    numb: 3,
+    question: "How many minutes are in a full week? ",
+    answer: "10,080",
+    options: ["10,060", "10,090", "10,070", "10,080"],
+  },
+  {
+    numb: 4,
+    question: "What is the 4th letter of the Greek alphabet?",
+    answer: "Delta",
+    options: ["Delta", "Beta", "Sigma", "Alpha"],
+  },
+  {
+    numb: 5,
+    question: "Which country below has won the most World Cups?",
+    answer: "Brazil",
+    options: ["Portugal", "Brazil", "France", "Argentina"],
+  },
+  {
+    numb: 6,
+    question: "Which country below has won the most World Cups?",
+    answer: "Brazil",
+    options: ["Portugal", "Brazil", "France", "Argentina"],
+  },
+];
